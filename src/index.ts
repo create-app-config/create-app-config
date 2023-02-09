b7ca134d0bdadfadfcbf9b0a4267a51406b00ceb
wesleyara <wes1gom@gmail.com>
2023-02-09 20:49:04 -0300

    feat: adding and using makeQuestions

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,37 @@
-console.log("hello world");
+import { ColorLog } from "./utils/colorLog";
+import {
+  makeInitialQuestions,
+  makeFinishQuestions,
+} from "./utils/makeQuestions";
+
+(async () => {
+  ColorLog.green("Welcome to the create-app-config CLI! 🚀");
+  ColorLog.green("Let's get started!");
+  console.log("");
+
+  const {
+    template,
+    isTypescript,
+    projectName,
+    packageManager,
+    linters,
+    husky,
+  } = await makeInitialQuestions();
+
+  let tailwind = "No";
+  if (template !== "Express") {
+    const response = await makeFinishQuestions();
+
+    tailwind = response.tailwind;
+  }
+
+  console.log(
+    template,
+    isTypescript,
+    projectName,
+    packageManager,
+    linters,
+    husky,
+    tailwind,
+  );
+})();
--- src/utils/makeQuestions.ts
+++ src/utils/makeQuestions.ts
@@ -1,14 +1,74 @@
 import { prompt } from "inquirer";
 
-interface IQuestions {
+export interface IQuestions {
   type: string;
   name: string;
   message: string;
-  choices: string[];
+  choices?: string[];
   default: string;
 }
 
-export const makeQuestions = async (questions: IQuestions[]) => {
+const questions: IQuestions[] = [
+  {
+    type: "list",
+    name: "template",
+    message: "What project template would you like to generate?",
+    choices: ["Reactjs", "Nextjs", "Vite", "Express"],
+    default: "React",
+  },
+  {
+    type: "list",
+    name: "isTypescript",
+    message: "Do you want to use Typescript?",
+    choices: ["Yes", "No"],
+    default: "Yes",
+  },
+  {
+    type: "input",
+    name: "projectName",
+    message: "What is the name of your project?",
+    default: "my-app",
+  },
+  {
+    type: "list",
+    name: "packageManager",
+    message: "What package manager would you like to use?",
+    choices: ["yarn", "npm"],
+    default: "yarn",
+  },
+  {
+    type: "list",
+    name: "linters",
+    message: "You want to use ESLint and Prettier?",
+    choices: ["Yes", "No"],
+    default: "Yes",
+  },
+  {
+    type: "list",
+    name: "husky",
+    message: "You want to use Husky?",
+    choices: ["Yes", "No"],
+    default: "Yes",
+  },
+];
+
+export const makeInitialQuestions = async () => {
+  const answers = await prompt(questions);
+
+  return answers;
+};
+
+export const makeFinishQuestions = async () => {
+  const questions: IQuestions[] = [
+    {
+      type: "list",
+      name: "tailwind",
+      message: "You want to use Tailwind CSS?",
+      choices: ["Yes", "No"],
+      default: "Yes",
+    },
+  ];
+
   const answers = await prompt(questions);
 
   return answers;
